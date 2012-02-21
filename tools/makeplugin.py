5edb3de96501b6c2c392d47abe8d504a16b00db0
Zhang Yungui <rhcad@hotmail.com>
2012-02-21 21:55:00 +0800

    Auto skip empty folders.

--- tools/makeplugin.py
+++ tools/makeplugin.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 Function:   Create a plugin project based on example project.
-            This script has been tested with ActivePython 2.7.
+            This script has been tested with ActivePython 2.7/3.2.
 
-Usage:      python makeplugin.py prjname [pkgname] [baseprj] [basepkg]
+Usage:      python makeplugin.py [prjname [pkgname [srcprj [srcpkg [useswig]]]]]
             or double click the file 'makeplugin.py'.
 
             prjname: name of the new project.
             pkgname: package name of the new project, the default value is 'example'.
-            baseprj: project name, the default value is 'pltempl'.
-            basepkg: package name of the template project, the default value is 'example'.
+            srcprj:  template (exists) project name, the default value is 'pltempl'.
+            srcpkg:  package name of the template project, the default value is 'example'.
+            useswig: need swig files or not (y/n), the default value is 'n'.
 
 Creator:    Zhang Yungui <rhcad@hotmail.com>
 Date:       2011.11.14
 ChangeList:
     1. Implemented the function: multi_replace, copyfiles, makeproj
     2. Auto copy interface files and skip swig files. [2011.12.14]
+    3. Auto skip empty folders. [2012.02.21]
 """
 
 import os, sys, re, uuid
@@ -27,26 +29,23 @@ def multi_replace(text, adict):
         return adict[match.group(0)]
     return rx.sub(xlat, text)
 
-def copyfiles(srcdir, dstdir, pairs, baseprj, callback, needswig=False):
-    if ".svn" in srcdir or "eclipse" in srcdir: return
-    if not needswig and "swig" in srcdir: return
-    has_ifile = not needswig and os.path.exists(os.path.join(srcdir, baseprj + ".i"))
-    
-    if not os.path.exists(dstdir):
-        os.makedirs(dstdir)
+def copyfiles(srcdir, dstdir, pairs, srcprj, callback, noswig):
+    if ".svn" in srcdir or not os.path.exists(srcdir): return
+    if noswig and "swig" in srcdir: return
+    has_i = noswig and os.path.exists(os.path.join(srcdir, srcprj + ".i"))
     
     for fn in os.listdir(srcdir):
         srcfile = os.path.join(srcdir, fn)
         dstfile = os.path.join(dstdir, multi_replace(fn, pairs))
         
         if os.path.isdir(srcfile):
-            copyfiles(srcfile, dstfile, pairs, baseprj, callback, needswig)
+            copyfiles(srcfile, dstfile, pairs, srcprj, callback, noswig)
             continue
         
-        if has_ifile and (fn == baseprj + ".i" or fn.startswith("test" + baseprj)): continue
+        if has_i and (fn==srcprj+".i" or fn.startswith("test"+srcprj)): continue
         
-        if os.path.isfile(srcfile) and not os.path.exists(dstfile) \
-                and callback(fn, pairs):
+        if not os.path.exists(dstfile) and callback(fn, pairs):
+            if not os.path.exists(dstdir): os.makedirs(dstdir)
             open(dstfile, "wb").write(open(srcfile, "rb").read())
             text = open(dstfile).read()
             newtext = multi_replace(text, pairs)
@@ -56,9 +55,9 @@ def copyfiles(srcdir, dstdir, pairs, baseprj, callback, needswig=False):
             else:
                 print('%s [created]' % (dstfile,))
 
-def makeproj(prjname, pkgname, baseprj, basepkg, needswig):
+def makeproj(prjname, pkgname, srcprj, srcpkg, noswig):
     rootpath = os.path.abspath('..')
-    basepath = os.path.join(rootpath, 'source', basepkg, baseprj)
+    basepath = os.path.join(rootpath, 'source', srcpkg, srcprj)
     pkgpath  = os.path.join(rootpath, 'source', pkgname)
     dstdir  = os.path.join(pkgpath, prjname)
     
@@ -71,27 +70,27 @@ def makeproj(prjname, pkgname, baseprj, basepkg, needswig):
     
     if not os.path.exists(pkgpath):
         os.makedirs(pkgpath)
-        srcmk = os.path.join(rootpath, 'source', basepkg, 'Makefile')
+        srcmk = os.path.join(rootpath, 'source', srcpkg, 'Makefile')
         if os.path.exists(srcmk):
             dstmk = os.path.join(pkgpath, 'Makefile')
             open(dstmk, "w").write(open(srcmk).read())
             print('%s [created]' % (dstmk,))
 
-    pairs = {baseprj:prjname, basepkg:pkgname}
+    pairs = {srcprj:prjname, srcpkg:pkgname}
     
     def matchfile(filename, pairs):
         if filename.find("_wrap.cxx") > 0 or filename.find("_wrap.h") > 0:
             return False
         return True
-    copyfiles(basepath, dstdir, pairs, baseprj, matchfile, needswig)
+    copyfiles(basepath, dstdir, pairs, srcprj, matchfile, noswig)
 
-    basepath = os.path.join(rootpath, 'interface', basepkg, baseprj)
+    basepath = os.path.join(rootpath, 'interface', srcpkg, srcprj)
     dstdir  = os.path.join(rootpath, 'interface', pkgname, prjname)
     intpairs = pairs
     intpairs["78d30c77-e0f0-48a3-a489-dd4327759c27"] = str(uuid.uuid1())
     intpairs["94071767-ba6b-4769-9eb4-2ebf469289f3"] = str(uuid.uuid1())
     intpairs["feefc399-29f2-4354-8eeb-048d4cf56567"] = str(uuid.uuid1())
-    copyfiles(basepath, dstdir, intpairs, baseprj, matchfile, needswig)
+    copyfiles(basepath, dstdir, intpairs, srcprj, matchfile, noswig)
 
     def matchproj(filename, pairs):
         if ".user" in filename: return False
@@ -100,7 +99,7 @@ def makeproj(prjname, pkgname, baseprj, basepkg, needswig):
                filename.startswith(key + '.'): return True
         return False
     projects = os.path.join(rootpath, 'projects')
-    copyfiles(projects, projects, pairs, baseprj, matchproj, needswig)
+    copyfiles(projects, projects, pairs, srcprj, matchproj, noswig)
 
 if __name__=="__main__":
     def inputparam(index, prompt, default=''):
@@ -110,10 +109,10 @@ if __name__=="__main__":
         return ret
     
     prjname = inputparam(1, 'New project name: ')
-    pkgname  = inputparam(2, 'Package name of the new project (default: example): ', 'example')
-    baseprj = inputparam(3, 'Template (exists) project name (default: pltempl): ', 'pltempl')
-    basepkg  = inputparam(4, 'Package name of the template project (default: example): ', 'example')
-    needswig = inputparam(5, 'Need swig (y/n) ? (default: n): ', 'n')
+    pkgname = inputparam(2, 'Package name of the new project (default: example): ', 'example')
+    srcprj  = inputparam(3, 'Template (exists) project name (default: pltempl): ', 'pltempl')
+    srcpkg  = inputparam(4, 'Package name of the template project (default: example): ', 'example')
+    useswig = inputparam(5, 'Need swig (y/n) ? (default: n): ', 'n')
     
-    makeproj(prjname, pkgname, baseprj, basepkg, 'y' in needswig)
+    makeproj(prjname, pkgname, srcprj, srcpkg, 'n' in useswig)
     if len(sys.argv) < 3: raw_input("Press <ENTER> to end.")
